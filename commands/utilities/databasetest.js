f246de2d49d60e9a4e58e26c8d8cd91a2fb82e57
Brick <robloxabood@gmail.com>
2026-02-12 17:54:42 +0300

    database namespaces feature
    fullly functional

--- commands/utilities/databasetest.js
+++ commands/utilities/databasetest.js
@@ -1,5 +1,5 @@
 //this comment only tests the database to see if it works through the number of times a user has used this command
-const keyv = require('../../database/keyv');
+const db = require('../../database/keyv');
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
@@ -8,11 +8,16 @@ module.exports = {
         .setDescription('Replies with Pong!'),
 
     async execute(interaction) {
-        const key = `uses:${interaction.user.id}`;
+        const userIdKey = interaction.user.id;
+        const count = (await db.uses.get(userIdKey)) ?? 0;
+        await db.uses.set(userIdKey, count + 1);
 
-        const count = (await keyv.get(key)) ?? 0;
-        await keyv.set(key, count + 1);
-
-        await interaction.reply(`You used this command ${count + 1} times.`);
+        /* testing new database namespaces feature
+        const threatID = interaction.user.id;
+        const threatValue = (await db.threats.get(threatID)) ?? 100;
+        await db.threats.set(threatID, threatValue + 1);
+        */
+ 
+        await interaction.reply(`You used this command ${count + 1} times and have a threat level of ${threatValue + 1}.`);
     },
 };
--- database/keyv.js
+++ database/keyv.js
@@ -3,12 +3,30 @@ const path =  require('path')
 const Keyv = require('keyv').default;
 const KeyvSqlite = require('@keyv/sqlite').default;
 const dbPath = path.join(__dirname, '..', 'shieldbot.sqlite');
-const keyv = new Keyv({
+
+//multiple table set up through namespaces
+const createStore = (namespace) => //we will create a new table for each type of key we want to store
+  new Keyv({
+    namespace,
     store: new KeyvSqlite({ uri: `sqlite://${dbPath}` })
 });
 
+/*
+//error handling
 keyv.on('error', err => {
     console.error('Keyv error:', err);
 });
+*/
 
-module.exports = keyv;
+module.exports = {
+  uses: createStore('uses'), //test uses table for databasetest.js
+  //threats: createStore('threats')
+};
+
+/* old single table set up
+//create "store" == "table"
+const keyv = new Keyv({ //this function creates a new table to the database specified
+    store: new KeyvSqlite({ uri: `sqlite://${dbPath}` })
+});
+
+//module.exports = keyv; //keyv is the table name*/
