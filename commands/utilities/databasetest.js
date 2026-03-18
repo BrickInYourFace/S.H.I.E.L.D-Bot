80b3ed4257d95baca995a39f6a4f56dab72199a1
leen <leenmoeenmadadha@gmail.com>
2026-03-19 00:04:47 +0300

    anything

--- commands/utilities/databasetest.js
+++ commands/utilities/databasetest.js
@@ -17,7 +17,7 @@ module.exports = {
         const threatValue = (await db.threats.get(threatID)) ?? 100;
         await db.threats.set(threatID, threatValue + 1);
         */
- 
+
         await interaction.reply(`You used this command ${count + 1} times and have a threat level of ${threatValue + 1}.`);
     },
 };
